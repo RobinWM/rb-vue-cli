9b76e112727ec1b1885c3fe374e8c0b55c7dacf5
RobinWM <302740969@qq.com>
2022-10-11 10:29:45 +0800

    feat: 入口文件修改

--- src/rb.js
+++ src/rb.js
@@ -1,7 +1,7 @@
 #! /usr/bin/env node
 const { program } = require('commander')
 const chalk = require('chalk')
-let figlet = require('figlet')
+const figlet = require('figlet')
 
 // 配置create命令
 program
