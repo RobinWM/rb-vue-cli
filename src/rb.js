48f99a6b9e11b1342bf3b08495e164322cdde287
RobinWM <302740969@qq.com>
2022-10-18 14:18:26 +0800

    fix: 忽略eslint规则

--- src/rb.js
+++ src/rb.js
@@ -1,43 +1,44 @@
 #! /usr/bin/env node
-const { program } = require('commander')
-const chalk = require('chalk')
-const figlet = require('figlet')
+const { program } = require("commander");
+const chalk = require("chalk");
+const figlet = require("figlet");
 
 // 配置create命令
 program
-  .command('create <app-name>')
-  .description('create a new project')
+  .command("create <app-name>")
+  .description("create a new project")
   // -f or --force 为强制创建，如果创建的目录存在则直接覆盖
-  .option('-f, --force', 'overwrite target directory if it exist')
+  .option("-f, --force", "overwrite target directory if it exist")
   .action((name, options) => {
     // 在 create.js 中执行创建任务
-    require('../lib/create.js')(name, options)
-  })
+    require("../lib/create.js")(name, options);
+  });
 
-program.on('--help', () => {
+program.on("--help", () => {
   // 使用 figlet 绘制 Logo
   console.log(
-    '\r\n' +
-      figlet.textSync('rb vue', {
-        font: 'Ghost',
-        horizontalLayout: 'default',
-        verticalLayout: 'default',
+    "\r\n" +
+      figlet.textSync("rb vue", {
+        font: "Ghost",
+        horizontalLayout: "default",
+        verticalLayout: "default",
         width: 80,
-        whitespaceBreak: true
+        whitespaceBreak: true,
       })
-  )
+  );
   // 新增说明信息
   console.log(
     `\r\nRun ${chalk.cyan(
       `rb <command> --help`
     )} for detailed usage of given command\r\n`
-  )
-})
+  );
+});
 
 program
   // 配置版本号信息
-  .version(`v${require('../package.json').version}`)
-  .usage('<command> [option]')
+  .version(`v${require("../package.json").version}`)
+  .usage("<command> [option]");
 
 // 解析用户执行命令传入参数
-program.parse(process.argv)
+// eslint-disable-next-line no-undef
+program.parse(process.argv);
